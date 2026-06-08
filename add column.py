01087cae9c1f5704315ab96bd3f5e8f58c0e6816
RZ <saniazafar193@gmail.com>
2026-06-08 06:19:45 +0500

    Add 'Salary' column to DataFrame
    Added a new column 'Salary' to the DataFrame.

--- add column.py
+++ add column.py
@@ -1,1 +1,9 @@
-
+# adding columns in dataframe
+import pandas as pd
+data = {"Name":["Ali","Raza"],
+        "Age":[20,21]}
+s = pd.DataFrame(data,index=["Employee no 1","Employee no 2"])
+print(s)
+# add new column
+s["Salary"] = [50000,40000]
+print(s)
